bbe03c39662629299291f0bf0fd42de482b4ae12
Colin McGraw <colinmcgraw@me.com>
2020-02-02 14:01:25 -0800

    checking object equality

--- api/sync-sheet.js
+++ api/sync-sheet.js
@@ -1,52 +1,59 @@
 const hubdbClient = require('../lib/hubdb-client');
 const gsheetClient = require('../lib/gsheet-client');
-const { getHubdbRowFromSheet, getRowValuesChanged } = require('../lib/data-transform');
+const { getHubdbRowFromSheet, getRowValueEquality } = require('../lib/data-transform');
 
 const hubdbTableId = '2054297';
 const sheetId = '1ELP2bRhfDs7QKHhdnnzVbH_7Q1R7LBjwYGqRJFhZvfg';
 
-let updatedRows = 0;
-let newRows = 0;
 
 module.exports = async (req, res) => {
+  let updatedRows = 0;
+  let newRows = 0;
 
+  console.log('Starting sync...');
   try {
     // all rows from sheet
     const locations = await gsheetClient.getLocationsFromSheet(sheetId);
+    console.log(`Got locations from sheet, ${locations.length} rows found...`);
 
-    locations.forEach(row => {
-      // convert to hubdb row format
+    // iterate over rows
+    for (let i=0; i < locations.length; i++) {
+      const row = locations[i];
       const hubdbValues = getHubdbRowFromSheet(row);
-      
-      (async () => {
-        // query table for existing match
-        const rowMatch = await hubdbClient.getFilteredTableRows(hubdbTableId, {
-          vendor_id: row.locationid,
-        });
 
-        // if a match exists
-        if (rowMatch.objects && rowMatch.objects.length === 1) {
-          const rowMatchVals = rowMatch.objects[0].values;
-          const rowMatchId = rowMatch.objects[0].id;
-          // check if any cell values have changed
-          const needsUpdate = getRowValuesChanged(hubdbValues, rowMatchVals);
+      // query table for existing match
+      const rowMatch = await hubdbClient.getFilteredTableRows(hubdbTableId, {
+        vendor_id: row.locationid,
+      });
 
-          if (needsUpdate) {
-            await hubdbClient.updateTableRow(hubdbTableId, rowMatchId, hubdbValues);
-            updatedRows++;
-          }
-        // if no match exists
-        } else {
-          // create a new hubdb row
-          await hubdbClient.addTableRow(hubdbTableId, hubdbValues);
-          newRows++;
+      // if a match exists
+      if (rowMatch.objects && rowMatch.objects.length === 1) {
+        const rowMatchVals = rowMatch.objects[0].values;
+        const rowMatchId = rowMatch.objects[0].id;
+        // check if any cell values have changed
+        const rowsIdentical = getRowValueEquality(hubdbValues, rowMatchVals);
+        console.log('Row values are identical: ', rowsIdentical);
+
+        if (!rowsIdentical) {
+          console.log('Updating row...');
+          await hubdbClient.updateTableRow(hubdbTableId, rowMatchId, hubdbValues);
+          console.log('Row updated: ', rowMatchId);
+          updatedRows++;
         }
+      // if no match exists
+      } else {
+        console.log('Creating row...');
+        // create a new hubdb row
+        const createdRow = await hubdbClient.addTableRow(hubdbTableId, hubdbValues);
+        console.log('Row created: ', createdRow.id);
+        newRows++;
+      }
+    } // end iteration
 
-      })();
-    }); //end forEach
-
+    console.log('Iteration finished, publishing table...');
     // publish table after updates
     await hubdbClient.publishTable(hubdbTableId);
+    console.log('Table published!');
 
     res.json({
       status: 'ok',
